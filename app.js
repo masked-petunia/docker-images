ed1dffefb56eea18a037bc90c29cc2b28515d8ef
Nazim Lachter <lachtater@gmail.com>
2018-08-29 11:46:52 +0200

    Host IP detection

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
 const fs = require('fs')
-const arpScanner = require('arpscan')
+const getMac = require('getmac').getMac
+const ip = require('ip')
+const arpScanner = require('arpscan/promise')
 
-const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+// -----------------------------------------------------------------------
 
-const syncAddresses = (error, list) => {
-    if(error) throw err
-    list.forEach(element => {
-        config.cluster.map(server => {
-            if(server.mac === element.mac) {
-                server.ip = element.ip
-            }
-            return server
+const handle = (name, action, param, callback) => {
+    action(param)
+        .then(callback)
+        .catch(() => {
+            console.log(`[ERROR] ${name}`)
         })
-    })
-    console.log(config.cluster)
 }
 
-arpScanner(syncAddresses, {
-    interface: config.network.interface,
-    args: [ config.network.host ],
-    sudo: true,
+const getMacAddress = () => new Promise((resolve, reject) => {
+    getMac((error, macAddress) => {
+        if(error) {
+            console.log(error)
+            reject(error)
+        } else {
+            resolve(macAddress)
+        }
+    })
+})
+
+// -----------------------------------------------------------------------
+
+const CONFIG = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+
+const arpScanOptions = { args: [ CONFIG.host.network ], sudo: true }
+if(CONFIG.host.interface) {
+    arpScanOptions.interface = CONFIG.host.interface
+}
+
+// -----------------------------------------------------------------------
+
+const defineNodeIP = (mac, ip) => {
+    CONFIG.nodes.map(node => {
+        if(mac === node.mac) {
+            node.ip = ip
+            console.log(`[NODE] ${node.name} is ${node.ip}`)
+        }
+        return node
+    })
+}
+
+// -----------------------------------------------------------------------
+
+// 1. Retrieving host MAC address
+handle('own-mac', getMacAddress, null, macAddress => {
+    
+    // 2. Adds the host IP if it is a node
+    defineNodeIP(macAddress, ip.address())
+
+    // 3. Search IPs on the network
+    handle('arp-scan', arpScanner, arpScanOptions, arpScanList => {
+
+        // 4. Adds the IPs of the corresponding nodes
+        arpScanList.forEach(element => {
+            defineNodeIP(element.mac, element.ip)
+        })
+
+    })
 })
