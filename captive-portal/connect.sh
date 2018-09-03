c8b531bed3180dd77c8d329f050ec6a94e956bf2
Nazim Lachter <lachtater@gmail.com>
2018-09-03 11:56:10 +0200

    Captive Portal v1.0

--- captive-portal/connect.sh
+++ captive-portal/connect.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "# Captive Portal Auto-Connect ($(date +'%m/%d/%y %H:%M:%S'))"
+echo "## Captive Portal Auto-Connect ##"
 
 connect() {
+  date=$(date +'%m/%d/%y %H:%M:%S')
   if command -v curl > /dev/null 2>&1 ; then
-    echo "-> Sending request using cURL"
+    echo "-> Sending request using cURL ($date)"
+
     curl --silent --insecure --max-time $CAPTIVE_TIMEOUT -o /dev/null \
       -X POST -d "$CAPTIVE_DATA" "$CAPTIVE_URL"
   else
-    echo "-> Sending request using wget"
+    echo "-> Sending request using wget ($date)"
     wget --quiet --no-check-certificate -O /dev/null \
       -T $CAPTIVE_TIMEOUT --post-data "$CAPTIVE_DATA" "$CAPTIVE_URL"
   fi
@@ -25,7 +27,14 @@ check() {
   fi
 }
 
-connect
-check
-
-echo "-------------------------------------------------"
+if [[ -z "${CAPTIVE_LOOP}" ]]; then
+  connect
+  check
+else
+  while true; do
+    echo "-------------------------------------------------"
+    connect
+    check
+    sleep $CAPTIVE_LOOP
+  done
+fi
